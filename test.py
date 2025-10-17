eb9e7216f77097c0793c8eab30df0ff919dad5a3
elisabetta-insureteach <elisabetta.chisso@insureteach.it>
2025-10-17 10:57:15 +0200

    results large instances

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from src.parse import parse_instance
+import csv
 from src.model import solutions
 from src.heuristics import lns_matheuristic, ils_matheuristic
+from src.utils import save_stats, get_parameters, configs
 from src.utils import configs, get_parameters, print_stats
 
 '''for i in range(1, 2):
@@ -17,8 +19,82 @@ from src.utils import configs, get_parameters, print_stats
 
 
 
+with open("results/results_50_6.csv", mode="a", newline="") as f:
+        writer = None
+        instance_path = f"dataset/r_50/r_50_70_30_70_6.txt"
+        folder_path = "dataset/r_50"
+        instance_name = 'r_50_70_30_70_6'
+        params = get_parameters(instance_path)
 
-instance_path = f"dataset/r_50/r_50_70_30_70_4.txt"
-params = get_parameters(instance_path)
-_ , _ , model_results_ils = ils_matheuristic(*params)
-print_stats(model_results_ils, title=f"Instance - ILS with No VI")
+        for name, (vi1, vi2, vi3, vi4) in configs.items():
+            stats = solutions(params, vi1, vi2, vi3, vi4)
+            if writer is None:
+                writer = csv.DictWriter(f, fieldnames=["Instance", "Algorithm", "Config"] + list(stats.keys()))
+                writer.writeheader()
+            save_stats(stats, writer, instance_name, "base", name)
+
+        _, _, stats_lns = lns_matheuristic(*params)
+        save_stats(stats_lns, writer, instance_name, "lns", "base")
+
+        _, _, stats_ils = ils_matheuristic(*params)
+        save_stats(stats_ils, writer, instance_name, "ils", "base")
+
+with open("results/results_50_7.csv", mode="a", newline="") as f:
+        writer = None
+        instance_path = f"dataset/r_50/r_50_70_30_70_7.txt"
+        folder_path = "dataset/r_50"
+        instance_name = 'r_50_70_30_70_7'
+        params = get_parameters(instance_path)
+
+        for name, (vi1, vi2, vi3, vi4) in configs.items():
+            stats = solutions(params, vi1, vi2, vi3, vi4)
+            if writer is None:
+                writer = csv.DictWriter(f, fieldnames=["Instance", "Algorithm", "Config"] + list(stats.keys()))
+                writer.writeheader()
+            save_stats(stats, writer, instance_name, "base", name)
+
+        _, _, stats_lns = lns_matheuristic(*params)
+        save_stats(stats_lns, writer, instance_name, "lns", "base")
+
+        _, _, stats_ils = ils_matheuristic(*params)
+        save_stats(stats_ils, writer, instance_name, "ils", "base")
+
+with open("results/results_50_8.csv", mode="a", newline="") as f:
+        writer = None
+        instance_path = f"dataset/r_50/r_50_70_30_70_8.txt"
+        folder_path = "dataset/r_50"
+        instance_name = 'r_50_70_30_70_8'
+        params = get_parameters(instance_path)
+
+        for name, (vi1, vi2, vi3, vi4) in configs.items():
+            stats = solutions(params, vi1, vi2, vi3, vi4)
+            if writer is None:
+                writer = csv.DictWriter(f, fieldnames=["Instance", "Algorithm", "Config"] + list(stats.keys()))
+                writer.writeheader()
+            save_stats(stats, writer, instance_name, "base", name)
+
+        _, _, stats_lns = lns_matheuristic(*params)
+        save_stats(stats_lns, writer, instance_name, "lns", "base")
+
+        _, _, stats_ils = ils_matheuristic(*params)
+        save_stats(stats_ils, writer, instance_name, "ils", "base")
+
+with open("results/results_50_9csv", mode="a", newline="") as f:
+        writer = None
+        instance_path = f"dataset/r_50/r_50_70_30_70_9.txt"
+        folder_path = "dataset/r_50"
+        instance_name = 'r_50_70_30_70_9'
+        params = get_parameters(instance_path)
+
+        for name, (vi1, vi2, vi3, vi4) in configs.items():
+            stats = solutions(params, vi1, vi2, vi3, vi4)
+            if writer is None:
+                writer = csv.DictWriter(f, fieldnames=["Instance", "Algorithm", "Config"] + list(stats.keys()))
+                writer.writeheader()
+            save_stats(stats, writer, instance_name, "base", name)
+
+        _, _, stats_lns = lns_matheuristic(*params)
+        save_stats(stats_lns, writer, instance_name, "lns", "base")
+
+        _, _, stats_ils = ils_matheuristic(*params)
+        save_stats(stats_ils, writer, instance_name, "ils", "base")
